435cfb067e526352086ad8df4e5b37c0d583e918
xia1997x <48220443+harukodi@users.noreply.github.com>
2024-10-17 15:16:20 +0200

    adding user and email to be able to create commits in github actions runner

--- create_release_gh_actions.py
+++ create_release_gh_actions.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, os
 set_new_version = None
 
 def set_release_version():
@@ -20,18 +20,18 @@ def set_release_version():
     
 
 def update_version_in_dev_branch():
+    set_user_identity = subprocess.run(['git', 'config', '--global', 'user.name', f'"{os.environ.get('GITHUB_ACTOR')}"'])
+    set_email = subprocess.run(['git', 'config', '--global', 'user.email', 'github-actions[bot]@users.noreply.github.com'])
     add_new_version_file_to_dev_branch = subprocess.run(['git', 'add', 'version.txt'], capture_output=True, text=True)
     commit_new_version_to_dev_branch = subprocess.run(['git', 'commit', '-m', f'"bump version to {set_new_version}"'], capture_output=True, text=True)
     push_new_version_to_dev_branch = subprocess.run(['git', 'push'], capture_output=True, text=True)
-    print(add_new_version_file_to_dev_branch)
-    print(commit_new_version_to_dev_branch)
-    print(push_new_version_to_dev_branch)
+    print(set_user_identity, set_email)
+    print(add_new_version_file_to_dev_branch, commit_new_version_to_dev_branch, push_new_version_to_dev_branch)
 
 def create_release_branch():
     create_release_branch = subprocess.run(['git', 'branch', f'release/{set_new_version}'], capture_output=True, text=True)
     push_release_branch = subprocess.run(['git', 'push', 'origin', f'release/{set_new_version}'], capture_output=True, text=True)
-    print(create_release_branch)
-    print(push_release_branch)
+    print(create_release_branch, push_release_branch)
 
 set_release_version()
 update_version_in_dev_branch()
