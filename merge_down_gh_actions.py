364800817bce7c2ef299c4f97b954b087b96360a
xia1997x <48220443+harukodi@users.noreply.github.com>
2024-10-22 13:34:40 +0200

    adding MergeDown python scripts/workflow file

---
+++ merge_down_gh_actions.py
@@ -0,0 +1,24 @@
+import subprocess, os
+release_version = None
+
+def fetch_release_version():
+    global release_version
+    try:
+        with open("version.txt", "r") as file:
+            release_version = file.read()
+    except FileNotFoundError as file_error:
+        print(f'File not Found: {file_error}')
+        exit()
+    except ValueError as value_error:
+        print(f'File is empty or is not an int: {value_error}')
+        exit()
+    
+def merge_release_branch_to_main():
+    checkout_to_main_branch = subprocess.run(["git", "checkout", "main"])
+    merge_release_branch_to_main = subprocess.run(["git", "merge", f"release/{release_version}"])
+    push_to_main = subprocess.run(["git", "push", "origin", "main"])
+    print(checkout_to_main_branch, merge_release_branch_to_main, push_to_main)
+    
+
+fetch_release_version()
+merge_release_branch_to_main()
