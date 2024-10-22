bc1fff0518e63173854efd00596a597eb89b88c2
None <github-actions[bot]@users.noreply.github.com>
2024-10-22 15:11:13 +0200

    minor fixes

--- git-scripts-gh-actions/merge_down_gh_actions.py
+++ git-scripts-gh-actions/merge_down_gh_actions.py
@@ -14,18 +14,14 @@ def fetch_release_version():
         exit()
     
 def merge_release_branch_to_main():
+    git_fetch = subprocess.run(["git", "fetch"])
+    git_pull = subprocess.run(["git", "pull"])
     github_actor = os.environ.get('GITHUB_ACTOR')
     set_user_identity = subprocess.run(['git', 'config', '--global', 'user.name', f'"{github_actor}"'])
     set_email = subprocess.run(['git', 'config', '--global', 'user.email', 'github-actions[bot]@users.noreply.github.com'])
-    git_fetch = subprocess.run(["git", "fetch"])
-    git_pull = subprocess.run(["git", "pull"])
-    checkout_to_main_branch = subprocess.run(["git", "switch", "main"])
+    checkout_to_main_branch = subprocess.run(["git", "checkout", "main"])
     merge_release_branch_to_main = subprocess.run(["git", "merge", f"origin/release/{release_version}", "--allow-unrelated-histories"])
-    git_add = subprocess(["git", "add", "."])
-    git_commit = subprocess.run(["git", "commit", "-m", f'"mergin release/{release_version} to main"'])
     push_to_main = subprocess.run(["git", "push"])
-    print(github_actor, set_user_identity, set_email ,git_fetch, git_pull, checkout_to_main_branch, merge_release_branch_to_main, git_add, git_commit, push_to_main)
-    
 
 fetch_release_version()
 merge_release_branch_to_main()
