99a06936ea16111d20d94b249de34380495fd12b
xia1997x <48220443+harukodi@users.noreply.github.com>
2024-10-22 13:53:57 +0200

    testing

--- git-scripts-gh-actions/merge_down_gh_actions.py
+++ git-scripts-gh-actions/merge_down_gh_actions.py
@@ -14,9 +14,9 @@ def fetch_release_version():
         exit()
     
 def merge_release_branch_to_main():
-    checkout_to_main_branch = subprocess.run(["git", "checkout", "main"])
     git_fetch = subprocess.run(["git", "fetch"])
     git_pull = subprocess.run(["git", "pull"])
+    checkout_to_main_branch = subprocess.run(["git", "checkout", "main"])
     merge_release_branch_to_main = subprocess.run(["git", "merge", f"release/{release_version}"])
     push_to_main = subprocess.run(["git", "push"])
     print(checkout_to_main_branch, merge_release_branch_to_main, push_to_main, git_fetch, git_pull)
